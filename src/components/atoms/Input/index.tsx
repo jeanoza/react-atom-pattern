739852d43c3c154c57b3d92f2b391f741cd5214c
jeanoza <choikb916@gmail.com>
2023-12-05 12:55:13 +0100

    tweaks: input label

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback } from 'react'
+import { ChangeEvent, SyntheticEvent, useCallback } from 'react'
 import { defaultInputStyle, defaultLabelStyle, defaultWrapperStyle } from './style'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -20,20 +20,39 @@ function Input(props: InputProps) {
     [setValue]
   )
 
+  const toggleFocusBlur = useCallback((e: SyntheticEvent<HTMLInputElement>) => {
+    const { target, type } = e
+
+    if (target) {
+      const { classList } = (target as HTMLElement).closest('div')?.querySelector('label') as HTMLElement
+      if (type === 'blur' && (target as HTMLInputElement).value === '') {
+        classList.add('text-base')
+      } else {
+        classList.remove('text-base')
+      }
+    }
+  }, [])
+
   return (
     <input
       {...restProps}
       className={`${defaultInputStyle} ${inputStyle ?? ''}`}
+      id={label}
       value={value}
       onChange={handleOnChange}
-      placeholder={placeholder ?? label}
+      onFocus={toggleFocusBlur}
+      onBlur={toggleFocusBlur}
     />
   )
 }
 
 function Label(props: InputProps) {
   const { label, labelStyle } = props
-  return <label className={`${defaultLabelStyle} ${labelStyle ?? ''}`}>{label}</label>
+  return (
+    <label htmlFor={label} className={`${defaultLabelStyle} ${labelStyle ?? ''}`}>
+      {label}
+    </label>
+  )
 }
 
 function InputWrapper(props: InputProps) {
--- src/components/atoms/Input/style.ts
+++ src/components/atoms/Input/style.ts
@@ -1,7 +1,8 @@
 const defaultWrapperStyle =
-  "relative flex gap-1 justify-between items-center py-4 px-3 border-2 rounded-md max-w-xs bg-inherit";
+  "relative flex gap-1 justify-between items-center pt-5 pb-1 px-2 border-2 rounded-md max-w-xs bg-inherit ";
 const defaultInputStyle = "outline-none bg-inherit w-full px-1";
 const defaultLabelStyle =
-  "absolute -top-2 left-2 px-2 capitalize bg-inherit dark:text-white ";
+  "absolute top-0 left-0 pl-3 pt-1 m-auto capitalize text-neutral-400 text-base transition-all";
+
 
 export { defaultInputStyle, defaultLabelStyle, defaultWrapperStyle };
